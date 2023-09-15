b197cb1f7b7a18d4bc2999d8993c2868785ff0c5
jadedgurl <126923594+jadedgurl@users.noreply.github.com>
2023-09-15 18:41:23 -0400

    Update main2.tf

--- main2.tf
+++ main2.tf
@@ -25,11 +25,6 @@ data "azurerm_resource_group" "rg" {
   name = var.resource_group_name
 }
 
-data "azurerm_virtual_network" "vnet" {
-  name                = var.virtual_network_name
-  resource_group_name = data.azurerm_resource_group.rg.name
-}
-
 data "azurerm_subnet" "snet" {
   name                 = var.subnet_name
   virtual_network_name = data.azurerm_virtual_network.vnet.name
